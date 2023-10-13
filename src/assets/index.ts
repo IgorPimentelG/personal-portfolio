38084b1333311ef97d5580044619b477479f5a79
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-10-13 10:38:54 -0300

    🍱 assets: add images

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as AWSLogo } from './aws-logo.png';
+export { default as TypescriptLogo } from './typescript-logo.png';
+export { default as DockerLogo } from './docker-logo.png';
+export { default as SpringLogo } from './spring-logo.png';
+export { default as JavaLogo } from './java-logo.png';
+export { default as Profile } from './profile.jpg';
